cb8efac04c85ccb87d81ca0868fd5f2be6859538
mablscmo <m.ontelius@hotmail.com>
2017-09-13 16:03:17 +0200

    Add files via upload

---
+++ Ex2.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 12:11:07 2017
+@author: monte
+"""
+from scipy import *
+from pylab import *
+import sys    
+
+import math 
+import numpy as np
+
+class Planet:
+    
+    
+    
+    def __init__(self,x,y,vx,vy,k=4*math.pi**2):
+        # Complete in exercise 2.2
+        self.t=0
+        self.x=x
+        self.y=y
+        self.vx=vx
+        self.vy=vy
+        self.k=k
+#        self.coord=array([[self.x],[self.y]],[[self.vx],[self.vy]])
+
+    def __repr__(self):
+        return'x={}, y={}, vx={}, vy={}'.format(self.x,self.y,self.vx,self.vy)
+
+    def doTimeStep(self,dt):
+        # Complete in exercise 2.2
+        self.vx+=-self.k*self.x/(((((self.x)**2)+(self.y)**2))**(3/2))
+        self.vy+=-self.k*self.y/(((((self.x)**2)+(self.y)**2))**(3/2))
+        self.x+=self.vx*dt
+        self.y+=self.vy*dt
+        self.t+=dt
+#        return(array([self.x,self.y],[self.vx,self.vy]))
+    def plot(self, maxt, dt):
+        xvalues=[self.x]
+        yvalues=[self.y]
+        for i in linspace(0, maxt, maxt/dt):
+            self.doTimeStep(dt)
+            xvalues.append(self.x)
+            yvalues.append(self.y)
+        return(plot(xvalues, yvalues, 'bx'))
+        
+#    def getPeriodAndAxis(self,dt):
+        # COmplete in exercise 2.4
