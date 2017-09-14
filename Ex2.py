45b36b06dd68cd28ae7da12dd123386c51809367
Pohad <oscar.pohad@gmail.com>
2017-09-14 14:40:46 +0200

    Update Ex2.py

--- Ex2.py
+++ Ex2.py
@@ -44,7 +44,28 @@ class Planet:
             xvalues.append(self.x)
             yvalues.append(self.y)
         (self.x,self.y,self.vx,self.vy)=(x,y,vx,vy)
-        return(plot(xvalues, yvalues))
+        return(plot(xvalues, yvalues, 'bx', markersize=1))
         
-#    def getPeriodAndAxis(self,dt):
-        # COmplete in exercise 2.4
+    def getPeriodAndAxis(self,maxt,dt):
+        xstart=self.x
+        (x,y,vx,vy)=(self.x,self.y,self.vx,self.vy)
+        self.t=0
+        for i in linspace(dt, maxt, maxt/dt):
+            self.doTimeStep(dt)
+            if self.x<0 and self.y<0:
+                break
+            else:
+                continue
+        T=(round(2*self.t, 3))
+        a=(round((abs(xstart)+abs(self.x))/2, 3))
+        (self.x,self.y,self.vx,self.vy)=(x,y,vx,vy)
+        return('Period is {} units, semi-major axis has a length of {} units, quotient is {}'.format(T,a,T**2/(a**3)))
+        
+#Tested with these planets    
+c=Planet(4,0,0,4) 
+print(c.getPeriodAndAxis(30,0.01))
+d=Planet(1,0,0,1)
+print(d.getPeriodAndAxis(100,.0001))
+
+
+
